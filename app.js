6ceea16c0e35fa5f867a82af5737a83419e6bccc
Maciej Zając <maciej.zajac.197@gmail.com>
2019-10-12 10:22:03 +0200

    code hotfix

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    // IMPORTANT
+    res.setHeader("Access-Controll-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use("/feed", feedRouter);
 
 app.listen(8080);
--- routes/feed.js
+++ routes/feed.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 
+router.post("/post", feedController.createPost);
+
 module.exports = router;
