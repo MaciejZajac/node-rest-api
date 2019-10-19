f72edc5c44f65cfb12f25534335278dfb9d13cfa
Maciej Zając <maciej.zajac.197@gmail.com>
2019-10-19 22:32:16 +0200

    rest api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const feedRouter = require("./routes/feed");
 
@@ -7,10 +9,11 @@ const app = express();
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     // IMPORTANT
-    res.setHeader("Access-Controll-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
@@ -18,4 +21,19 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRouter);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log("error", error);
+    const status = error.statusCode | 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+});
+
+mongoose
+    .connect("mongodb+srv://maciej:132639@cluster0-m9slc.mongodb.net/messages?retryWrites=true&w=majority")
+    .then(result => {
+        console.log("sukces");
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const { body } = require("express-validator/check");
 const feedController = require("../controllers/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+    "/post",
+    [
+        body("title")
+            .trim()
+            .isLength({ min: 5, max: 30 }),
+        body("content")
+            .trim()
+            .isLength({ min: 5, max: 200 })
+    ],
+    feedController.createPost
+);
+
+router.get("/post/:postId", feedController.getPost);
 
 module.exports = router;
