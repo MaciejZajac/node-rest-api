5aa04357c81439883f49e98e6c4206ba90f1ab29
Maciej Zając <maciej.zajac.197@gmail.com>
2019-10-22 21:50:13 +0200

    deleting a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -127,6 +127,33 @@ exports.updatePost = (req, res, next) => {
         });
 };
 
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error("No file picked.");
+                error.statusCode = 404;
+                throw error;
+            }
+            // Check if logged in user is the owner of the post.
+            clearImage(post.imageUrl);
+
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            res.status(200).json({
+                message: "Deleted post."
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
 const clearImage = filePath => {
     filePath = path.join(__dirname, "..", filePath);
     fs.unlink(filePath, err => console.log(err));
